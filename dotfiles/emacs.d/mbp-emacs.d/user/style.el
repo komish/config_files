0fb42ee39fdfb8fcc7c13ed9d5936e0126b6d450
Jose R. Gonzalez <Komish@users.noreply.github.com>
2019-11-13 00:01:04 -0600

    adjusting fonts, todo sequence

--- dotfiles/emacs.d/mbp-emacs.d/user/style.el
+++ dotfiles/emacs.d/mbp-emacs.d/user/style.el
@@ -1,10 +1,10 @@
 ;; configure font
-(set-default-font "Fantasque Sans Mono-14")
+(set-default-font "Victor Mono-14")
 
 ;; configure org levels
 (custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.25))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.25))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.10))))
 ;; '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
 ;; '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
 ;; '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
